30393e339344704499b28ea200c837a4879c2d56
Kirill <pitometsk@gmail.com>
2023-05-14 18:35:41 +0300

    Made a new Api connection. Added try/catch

--- client/src/api/Note.js
+++ client/src/api/Note.js
@@ -1,87 +1,59 @@
-const NOTE_ENTITY_ID = 'dcUCkdW61cT4oAW6tdKmoI'
-const DB_ID = 'crWRqxW5jdLyoynwPFifiW'
-const API_KEY = 'brWOxdQYnlgyoAWPtdU8o_'
-const DB_API = `https://quintadb.com/apps/${DB_ID}`
+import axios from 'axios'
+// ==== Utils ====
+import { identifyError } from '../utils/identifyError'
+
+const NOTES_API_URL = `http://localhost:7777/api/notes`
+const $notes_api = axios.create({
+	baseURL: NOTES_API_URL,
+	headers: { 'Content-Type': 'application/json' },
+})
 
 class NotesApi {
 	static async getAll() {
-		const fetchUrl = `${DB_API}/dtypes/entity/${NOTE_ENTITY_ID}.json?`
-		const params = new URLSearchParams({
-			rest_api_key: API_KEY,
-			name_value: 1,
-			page: 1,
-			per_page: 200,
-		})
+		try {
+			const res = await $notes_api.get('get')
 
-		const res = await fetch(fetchUrl + params, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		}).then(async data => await data.json())
-
-		return res.records
+			return res.data
+		} catch (err) {
+			return identifyError(err)
+		}
 	}
 
 	static async getOne(id) {
-		const fetchUrl = `${DB_API}/dtypes/${id}.json?`
-		const params = new URLSearchParams({
-			rest_api_key: API_KEY,
-			name_value: 1,
-		})
-
-		const res = await fetch(fetchUrl + params, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		}).then(async data => await data.json())
-
-		return res.record
+		try {
+			const res = await $notes_api.get(`get/${id}`)
+			return res.data
+		} catch (err) {
+			return identifyError(err)
+		}
 	}
 
 	static async edit(id, values) {
-		const fetchUrl = `${DB_API}/dtypes/${id}.json?`
-		const jsonValues = JSON.stringify(values)
-		const params = new URLSearchParams({
-			rest_api_key: API_KEY,
-			json_values: jsonValues,
-		})
-
-		await fetch(fetchUrl + params, {
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
+		try {
+			await $notes_api.put(`edit/${id}`, values)
+		} catch (err) {
+			console.log(err.response?.data.message)
+			return identifyError(err)
+		}
 	}
 
 	static async delete(id) {
-		const fetchUrl = `${DB_API}/dtypes/${id}.json?`
-		const params = new URLSearchParams({
-			rest_api_key: API_KEY,
-		})
-
-		await fetch(fetchUrl + params, {
-			method: 'DELETE',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
+		try {
+			await $notes_api.delete(`delete/${id}`)
+		} catch (err) {
+			console.log(err.response?.data.message)
+			return identifyError(err)
+		}
 	}
 
 	static async create(values) {
-		const fetchUrl = `${DB_API}/dtypes.json`
-		const jsonData = JSON.stringify({ ...values, entity_id: NOTE_ENTITY_ID })
-		const params = new URLSearchParams({
-			rest_api_key: API_KEY,
-			json_values: jsonData,
-		})
-
-		await fetch(fetchUrl + params, {
-			method: 'POST',
-			json_values: jsonData
-		})
+		try {
+			const res = await $notes_api.post(`create`, values)
+			return res.data
+		} catch (err) {
+			console.log(err.response?.data.message)
+			return identifyError(err)
+		}
 	}
 }
 
