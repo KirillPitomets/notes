c4982e7dda25bb43984dd666d2cb4b0f1d56c462
Kirill <pitometsk@gmail.com>
2023-05-14 18:06:03 +0300

    Made content for Modal that can delete a note

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -8,15 +8,25 @@ import Header from '../components/Header/Header'
 import Sidebar from '../components/Sidebar/Sidebar'
 import Workspace from '../components/Workspace/Workspace'
 import Modal from '../components/Modal/Modal'
+import Button from '../components/UI/Button/Button'
+import { useDebounce } from '../components/useDebounce'
 
 const App = () => {
-	const { isModalDisplay, handleModal } = useNotes()
-	
+	const { isModalDisplay, handleModal, fetchDelete, note } = useNotes()
+	const [deleteNoteDebounce, isLoading] = useDebounce(async () => {
+  await fetchDelete(note.id)
+		handleModal()
+	}, 500)
+
 	return (
 		<>
-			<Modal isDisplay={isModalDisplay} handleModal={handleModal}>
-
-
+			<Modal isModalDisplay={isModalDisplay} handleModal={handleModal}>
+				Are you sure you want to remove this note ?
+				<div className={cl.buttons}>
+					<Button onClick={deleteNoteDebounce}>Delete</Button>
+					<Button onClick={handleModal}>Cancel</Button>
+				</div>
+				{isLoading && <span>Loading...</span>}
 			</Modal>
 
 			<Header />
